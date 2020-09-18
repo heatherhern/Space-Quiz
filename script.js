2228ae5cbdf44a213ff5674bbac500decb01dc4f
Heather Hern <heathernewlin1@gmail.com>
2020-09-18 17:38:47 -0400

    Added initials local storage

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const scoreDisplay = document.getElementById('score');
 var timeEl = document.querySelector(".time");
 const endContainer = document.getElementById('end-container');
 const restartButton = document.getElementById('restart-btn');
-const mostRecentScore = localStorage.getItem('mostRecentScore'); 
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('final-score');
 const initialsInput = document.getElementById('initalsInput');
+const initialsButton = document.getElementById('initials-btn');
 finalScore.innerText = mostRecentScore
 var secondsLeft = 5;
 const correctScoreValue = 10
@@ -19,17 +20,75 @@ let userInitials = ""
 var initialsDisplay = document.getElementById('final-initials');
 let shuffledQuestions, currentQuestionIndex
 
+// Questions object //
+
+const questions = [
+    {
+        question: 'What makes up most of the mass in our solar system?',
+        answers: [
+            { text: 'The Sun', correct: true },
+            { text: 'The Planets', correct: false }
+        ]
+    },
+    {
+        question: 'Which planet is home to the solar systems tallest volcano?',
+        answers: [
+            { text: 'Earth', correct: false },
+            { text: 'Mars', correct: true },
+            { text: 'Mercury', correct: false },
+            { text: 'Saturn', correct: false }
+        ]
+    },
+    {
+        question: 'Which planet is home to the solar systems tallest volcano?',
+        answers: [
+            { text: 'Earth', correct: false },
+            { text: 'Mars', correct: true },
+            { text: 'Mercury', correct: false },
+            { text: 'Saturn', correct: false }
+        ]
+    },
+    {
+        question: 'How many times could Earth fit inside Jupiter?',
+        answers: [
+            { text: '400', correct: false },
+            { text: '1,200', correct: false },
+            { text: '1,000', correct: true },
+            { text: '10,000', correct: false }
+        ]
+    },
+    {
+        question: 'Which planet is the coldest?',
+        answers: [
+            { text: 'Pluto', correct: false },
+            { text: 'Neptune', correct: false },
+            { text: 'Jupiter', correct: false },
+            { text: 'Uranus', correct: true }
+        ]
+    },
+    {
+        question: 'How many "Earth-years" does it take for Neptune to orbit the Sun?',
+        answers: [
+            { text: '15', correct: false },
+            { text: '301', correct: false },
+            { text: '89', correct: false },
+            { text: '165', correct: true }
+        ]
+    }
+]
+
+
 // TIMER CONTENT //
 
 function setTime() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds left till game over";
 
-    if(secondsLeft <= 0) {
-    clearInterval(timerInterval);
-    openEndContainer();
-    }
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            openEndContainer();
+        }
 
     }, 1000);
 }
@@ -50,6 +109,33 @@ nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
 })
+
+
+
+
+initialsButton.addEventListener('click', initialsFunction)
+
+function initialsFunction() {
+
+    if (initialsInput.value.length < 1) return;
+
+    initialsInput.innerHTML += '<li>' + initialsInput.value + '</li>';
+
+    // Save the list to localStorage
+    localStorage.setItem('recentInitials', initialsInput.value);
+    console.log(initialsInput.value);
+
+    var saved = localStorage.getItem('recentInitials');
+
+    // If there are any saved items, update our list
+    if (saved) {
+        initialsDisplay.innerHTML = saved;
+
+        // Clear input
+        initialsInput.value = '';
+
+    }
+}
 
 function startGame() {
     startButton.classList.add('hide') // hides the start button //
@@ -98,17 +184,18 @@ function selectAnswer(e) {
     if (selectedButton.dataset.correct) {
         incrementScore(correctScoreValue);
     }
+}
 
 // opens end page if there aren't any questions left //
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide')
-    } else {
-        openEndContainer()
-        localStorage.setItem("mostRecentScore", score);
-        localStorage.setItem("mostRecentInitials", userInitials);
-        initialsDisplay.innerText = userInitials;
-    }
+if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+} else {
+    openEndContainer()
+    localStorage.setItem("mostRecentScore", score);
+    // localStorage.setItem("mostRecentInitials", userInitials);
+    // initialsDisplay.innerText = userInitials;
 }
+
 
 // Increases score throughout game //
 function incrementScore(num) {
@@ -141,56 +228,3 @@ function restartQuiz() {
 
 // Questions Object //
 
-const questions = [
-    {
-    question: 'What makes up most of the mass in our solar system?',
-    answers: [
-        { text: 'The Sun', correct: true },
-        { text: 'The Planets', correct: false }
-    ]
-    },
-    {
-    question: 'Which planet is home to the solar systems tallest volcano?',
-    answers: [
-        { text: 'Earth', correct: false },
-        { text: 'Mars', correct: true },
-        { text: 'Mercury', correct: false },
-        { text: 'Saturn', correct: false }
-    ]
-    },
-    {
-    question: 'Which planet is home to the solar systems tallest volcano?',
-    answers: [
-        { text: 'Earth', correct: false },
-        { text: 'Mars', correct: true },
-        { text: 'Mercury', correct: false },
-        { text: 'Saturn', correct: false }
-    ]
-    },
-    {
-    question: 'How many times could Earth fit inside Jupiter?',
-    answers: [
-        { text: '400', correct: false },
-        { text: '1,200', correct: false },
-        { text: '1,000', correct: true },
-        { text: '10,000', correct: false }
-    ]
-    },
-    {
-    question: 'Which planet is the coldest?',
-    answers: [
-        { text: 'Pluto', correct: false },
-        { text: 'Neptune', correct: false },
-        { text: 'Jupiter', correct: false },
-        { text: 'Uranus', correct: true }
-    ]
-    },
-    {
-    question: 'How many "Earth-years" does it take for Neptune to orbit the Sun?',
-    answers: [
-        { text: '15', correct: false },
-        { text: '301', correct: false },
-        { text: '89', correct: false },
-        { text: '165', correct: true }
-    ]
-    }]
